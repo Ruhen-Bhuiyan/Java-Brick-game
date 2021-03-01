e129a312bb0e273a4db7eb90bede0905b4fbf547
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-03-01 21:29:37 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import javax.swing.JFrame;
 public class Main {
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		JFrame object = new JFrame();
 		Game game=new Game();
